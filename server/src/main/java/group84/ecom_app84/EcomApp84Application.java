aba6beba80481ce2b0eaa4254e66e7c2babc0411
chandanabl9 <chandanabelur2004@gmail.com>
2025-09-17 09:40:07 +0530

    first commit

---
+++ server/src/main/java/group84/ecom_app84/EcomApp84Application.java
@@ -0,0 +1,13 @@
+package group84.ecom_app84;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApp84Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApp84Application.class, args);
+	}
+
+}
---
+++ server/src/test/java/group84/ecom_app84/EcomApp84ApplicationTests.java
@@ -0,0 +1,13 @@
+package group84.ecom_app84;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApp84ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
